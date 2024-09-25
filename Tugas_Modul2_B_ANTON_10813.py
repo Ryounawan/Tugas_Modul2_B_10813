43543975b986dfab756de043897399f5ee4b7257
Newbie <113039377+Ryounawan@users.noreply.github.com>
2024-09-25 22:51:55 +0700

    Update Tugas_Modul2_B_ANTON_10813.py

--- Tugas_Modul2_B_ANTON_10813.py
+++ Tugas_Modul2_B_ANTON_10813.py
@@ -5,10 +5,10 @@ import os
 
 
 #Load model yang sudah dibuat dari Jupyter Notebook
-model_path = 'GBT_heartDisease_model.pkl'
+model = 'GBT_heartDisease_model.pkl'
 
 
-with open(model_path,'rb') as f:
+with open(model,'rb') as f:
     loaded_model = pickle.load(f)
 
 
